b9d55c5a664e7f5311fc83f252752b787de9ef31
chrishuhyc <chrishuhyc@gmail.com>
2020-03-29 17:23:42 +0800

    one to one

--- mybatis/db/test.sql
+++ mybatis/db/test.sql
@@ -1,8 +1,22 @@
 use mysql;
+drop table user_test;
 CREATE table user_test
 (
-    id       int(11) ,
+    id       int(11),
     username varchar(50),
-    password varchar(50)
+    password varchar(50),
+    unique key (id)
 ) engine = innodb;
-insert into user_test (id, username,password) values(1, 'User1','123456');
+insert into user_test (id, username, password)
+values (1, 'User1', '123456');
+
+drop table order_test;
+CREATE table order_test
+(
+    uid       int(11),
+    ordertime bigint(13),
+    total     int(10),
+    FOREIGN KEY (uid) references user_test (id)
+) engine = innodb;
+insert into order_test (uid, ordertime, total)
+values (1,1585473694 , 1);
--- mybatis/src/main/java/start/pojo/User.java
+++ mybatis/src/main/java/start/pojo/User.java
@@ -2,11 +2,14 @@ package start.pojo;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class User {
 
     private Integer id;
     private String username;
     private String password;
+    private Date birthday;
 
 }
