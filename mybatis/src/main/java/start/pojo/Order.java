76fe3806c4a3d12743d8119b691dd7b4d8ca0b0d
chrishuhyc <chrishuhyc@gmail.com>
2020-03-29 20:46:42 +0800

    two cache

--- mybatis/src/main/java/start/pojo/Order.java
+++ mybatis/src/main/java/start/pojo/Order.java
@@ -2,9 +2,10 @@ package start.pojo;
 
 import demo.pojo.User;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Order {
+public class Order implements Serializable {
     private long uid;
     private Date ordertime;
     private double total;
