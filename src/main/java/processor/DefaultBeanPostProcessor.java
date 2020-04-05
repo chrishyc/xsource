1db6e1f98267c1be4b6c827acc60380395faf444
chrishuhyc <chrishuhyc@gmail.com>
2020-04-05 22:54:46 +0800

    processpr

---
+++ src/main/java/processor/DefaultBeanPostProcessor.java
@@ -0,0 +1,24 @@
+package processor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+//@Component
+public class DefaultBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if ("dataSource".equals(beanName)) {
+        }
+        System.out.println("BeanPostProcessor 实现类 postProcessBeforeInitialization 方法被调用中......");
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if ("accountDao".equals(beanName)) {
+        }
+        System.out.println("BeanPostProcessor 实现类 postProcessAfterInitialization 方法被调用中......");
+        return bean;
+    }
+}
