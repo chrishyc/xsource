0357f20a8669bb97b3d8e46a45090bf5692f5646
chrishuhyc <chrishuhyc@gmail.com>
2020-04-11 19:49:54 +0800

    bean实例化感知接口

--- src/main/java/aop/Sample.java
+++ src/main/java/aop/Sample.java
@@ -1,5 +1,6 @@
 package aop;
 
+import net.sf.cglib.core.DebuggingClassWriter;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -17,6 +18,12 @@ public class Sample {
     public void testAnnotationAop() {
         ApplicationContext ac = new AnnotationConfigApplicationContext(Config.class);
         Target2 target = ac.getBean(Target2.class);
+        target.transfer();
+    }
+    public static void main(String[] args){
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "/Users/chris/xsource/src/main/java/aop/");
+        ApplicationContext ac = new ClassPathXmlApplicationContext("aop.xml");
+        Target target = ac.getBean(Target.class);
         target.transfer();
     }
 }
