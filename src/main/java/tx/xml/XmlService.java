d08fe4c92f9aaa0daa174aa5fa159d34d13b3061
chris <chrishuhyc@gmail.com>
2020-04-12 16:30:59 +0800

    内部bean命名策略

---
+++ src/main/java/tx/xml/XmlService.java
@@ -0,0 +1,11 @@
+package tx.xml;
+
+public class XmlService {
+    public void transfer() {
+        System.out.println("XmlService.transfer");
+    }
+
+    public void query() {
+        System.out.println("XmlService.query");
+    }
+}
