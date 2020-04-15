e84c5c529470d8841aaf753b8e372ec70c386d65
chrishuhyc <chrishuhyc@gmail.com>
2020-04-15 16:34:56 +0800

    exception handler

--- src/main/java/mvc/controller/RedirectController.java
+++ src/main/java/mvc/controller/RedirectController.java
@@ -1,11 +1,25 @@
 package mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Controller
 public class RedirectController {
+    
+    @ExceptionHandler(ArithmeticException.class)
+    public void handleException(ArithmeticException exception, HttpServletResponse response) {
+        // 异常处理逻辑
+        try {
+            response.getWriter().write(exception.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     
     @RequestMapping("/handleRedirect")
     public String handleRedirect(String name, RedirectAttributes
