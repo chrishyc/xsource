cf512f1913f5bb36bec5bf187402c50994b2d160
chrishuhyc <chrishuhyc@gmail.com>
2020-04-18 19:02:54 +0800

    mvc注解驱动

--- src/main/java/mvc/Sample.java
+++ src/main/java/mvc/Sample.java
@@ -1,6 +1,8 @@
 package mvc;
 
 import org.junit.Test;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.w3c.dom.Element;
 
 public class Sample {
     /**父子webApplicationContext
@@ -14,6 +16,32 @@ public class Sample {
      */
     @Test
     public void testInitWebContext(){
+    
+    }
+    
+    /**
+     * {@link org.springframework.web.servlet.config.AnnotationDrivenBeanDefinitionParser#parse(Element, ParserContext)}
+     * 开启mvc注解驱动后，会配置
+     * RequestMappingHandlerMapping
+     * conversionService(default:FormattingConversionServiceFactoryBean)
+     * validator(default:OptionalValidatorFactoryBean)
+     * ConfigurableWebBindingInitializer
+     * RequestMappingHandlerAdapter
+     * ExceptionHandlerExceptionResolver
+     * ResponseStatusExceptionResolver
+     * DefaultHandlerExceptionResolver
+     */
+    @Test
+    public void testMVCBeanDefinitionParser(){
+    
+    }
+    
+    /**
+     * mvc 注册component扫描器后会解析注解，默认在
+     * {@link org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider#registerDefaultFilters}
+     * 会注册@Component，只要注解上有@Component,都会被解析生成对应的Bean实例
+     */
+    public void testComponentScan(){
     
     }
 }
