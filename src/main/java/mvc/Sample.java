e001ff4de2bfdf3455523b3fe520d68ea39b5e1b
chrishuhyc <chrishuhyc@gmail.com>
2020-04-19 17:01:44 +0800

    返回有ResponseBody注解的返回值

--- src/main/java/mvc/Sample.java
+++ src/main/java/mvc/Sample.java
@@ -1,26 +1,26 @@
 package mvc;
 
+import mvc.controller.ReturnTypeController;
 import org.junit.Test;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.w3c.dom.Element;
-import mvc.controller.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Sample {
-    /**父子webApplicationContext
+    /**
+     * 父子webApplicationContext
      * 1.{@link org.springframework.web.context.ContextLoaderListener#contextInitialized}
      * 会监听ServletContext初始化回调，查看是否有contextClass，有则加载。此处的contextClass为父webApplicationContext
-     *
+     * <p>
      * 2.{@link org.springframework.web.servlet.DispatcherServlet}父类
      * {@link org.springframework.web.servlet.FrameworkServlet#createWebApplicationContext}会生成一个
      * 子类webApplicationContext，即XmlWebApplicationContext
-     *
      */
     @Test
-    public void testInitWebContext(){
+    public void testInitWebContext() {
     
     }
     
@@ -37,7 +37,7 @@ public class Sample {
      * DefaultHandlerExceptionResolver
      */
     @Test
-    public void testMVCBeanDefinitionParser(){
+    public void testMVCBeanDefinitionParser() {
     
     }
     
@@ -47,11 +47,12 @@ public class Sample {
      * 会注册@Component，只要注解上有@Component,都会被解析生成对应的Bean实例
      */
     @Test
-    public void testComponentScan(){
+    public void testComponentScan() {
     
     }
     
-    /**初始化过程:
+    /**
+     * 初始化过程:
      * 1.spring mvc默认HandlerMapping为RequestMappingHandlerMapping
      * 2.注册RequestMappingHandlerMapping时机为
      * {@link org.springframework.web.servlet.config.AnnotationDrivenBeanDefinitionParser#parse(Element, ParserContext)}
@@ -60,17 +61,17 @@ public class Sample {
      * 4.获取bean容器所有bean去判断bean是否是handler,{@link org.springframework.web.servlet.handler.AbstractHandlerMethodMapping#processCandidateBean(String)}
      * ```
      * protected boolean isHandler(Class<?> beanType) {
-     * 		return (AnnotatedElementUtils.hasAnnotation(beanType, Controller.class) ||
-     * 				AnnotatedElementUtils.hasAnnotation(beanType, RequestMapping.class));
-     *        }
+     * return (AnnotatedElementUtils.hasAnnotation(beanType, Controller.class) ||
+     * AnnotatedElementUtils.hasAnnotation(beanType, RequestMapping.class));
+     * }
      * ```
-     *
+     * <p>
      * 5.RequestMappingHandlerMapping.createRequestMappingInfo扫描含有RequestMapping的方法
      * 6.RequestMappingHandlerMapping.registerHandlerMethod完成mapping生成过程.
      * 7.AbstractApplicationContext.finishRefresh时发起publishEvent,导致DispatcherServlet刷新初始化initStrategies,此时会初始化
      * DispatcherServlet的handlerMapping,DispatcherServlet.initHandlerMappings
-     *
-     *
+     * <p>
+     * <p>
      * 请求过程:
      * 8.请求到来时，会进入DispatcherServlet.doDispatch，决定对应的handler,会进入DispatcherServlet.getHandler
      * 9.进入AbstractHandlerMethodMapping.lookupHandlerMethod获取对应的handler
@@ -80,7 +81,7 @@ public class Sample {
      * 12.RequestMappingHandlerAdapter.invokeHandlerMethod执行最后的方法逻辑
      */
     @Test
-    public void testHandlerMapping(){
+    public void testHandlerMapping() {
     
     }
     
@@ -93,32 +94,36 @@ public class Sample {
      * 获取默认method参数解析器.getDefaultArgumentResolvers
      * 获取默认method参数绑定器.getDefaultInitBinderArgumentResolvers
      * 获取默认method返回值处理器.getDefaultReturnValueHandlers
-     *
+     * <p>
      * 1.如何确定需要绑定哪些参数?
-     *
+     * <p>
      * 2.这些参数如何绑定的?
      */
     @Test
-    public void testMethodParam(){
+    public void testMethodParam() {
     
     }
     
-    /**Represents a model and view returned by a handler
+    /**
+     * Represents a model and view returned by a handler
      * {@link org.springframework.web.servlet.HandlerAdapter#handle(HttpServletRequest, HttpServletResponse, Object)}
-     *
+     * <p>
      * DispatcherServlet.doDispatch
      * // Actually invoke the handler. handlerAdapter处理完handler逻辑会返回一个ModelAndView
      * mv = ha.handle(processedRequest, response, mappedHandler.getHandler());
-     *
+     * <p>
      * 1.什么时候返回ModelAndView？什么时候返回其他结果?
      * 2.返回void怎么处理?
-     *
-     * 1.有返回值且返回类型为ModelAndView，{@link ParamMappingController#sayHello()}
-     * 2.返回void时{@link ParamMappingController#returnVoid()}
-     * 3.返回string时{@link ParamMappingController#returnString()}
+     * <p>
+     * 1.有返回值且返回类型为ModelAndView，{@link ReturnTypeController#returnModelAndView()}
+     * 2.返回void时{@link ReturnTypeController#returnVoid()}
+     * 3.返回string时{@link ReturnTypeController#returnString()}
+     * 4.返回responseBody时{@link ReturnTypeController#returnResponseBody}
      */
     @Test
-    public void testReturn(){
+    public void testReturn() {
     
     }
+    
+    
 }
