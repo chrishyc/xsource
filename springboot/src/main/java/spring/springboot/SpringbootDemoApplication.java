1834984240ae17ec326a456baea5e94deb2c086e
chris <chrishuhyc@gmail.com>
2020-04-25 21:00:23 +0800

    spring boot run报错原因

--- springboot/src/main/java/spring/springboot/SpringbootDemoApplication.java
+++ springboot/src/main/java/spring/springboot/SpringbootDemoApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 /**
  * @author chris
+ * springboot以module形式run无反应,发现原因:
+ * 1.不是以maven项目运行,add as maven
+ * 2.运行起来报错 Unable to start ServletWebServerApplicationContext due to missing ServletWebServerFactory bean
+ * 因为springboot2.0+需要tomcat8.5+版本
  */
 @SpringBootApplication
 public class SpringbootDemoApplication extends SpringBootServletInitializer {
