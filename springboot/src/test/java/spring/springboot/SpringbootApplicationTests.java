8746d5bc43c5e92278aef455b605fd71823b355c
chris <chrishuhyc@gmail.com>
2020-04-25 22:10:23 +0800

    springboot 文件配置application.properties

--- springboot/src/test/java/spring/springboot/SpringbootApplicationTests.java
+++ springboot/src/test/java/spring/springboot/SpringbootApplicationTests.java
@@ -1,19 +1,33 @@
 package spring.springboot;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import spring.springboot.controller.DemoController;
+import spring.springboot.pojo.Person;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringbootApplicationTests {
 	
 	@Autowired
 	private DemoController demoController;
+	
+	@Autowired
+	private Person person;
 
 	@Test
 	void contextLoads() {
 		demoController.sayHello();
 	}
+	
+	@Test
+	public void testConfigurationProperties(){
+		System.out.println(person);
+		System.out.println(person);
+	}
+	
 
 }
