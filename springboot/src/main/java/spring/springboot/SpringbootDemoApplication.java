b879a8b170200971758291f37e2b2d00f37900e9
chris <chrishuhyc@gmail.com>
2020-04-27 22:50:43 +0800

    springboot @EnableAutoConfiguration自动配置源码分析

--- springboot/src/main/java/spring/springboot/SpringbootDemoApplication.java
+++ springboot/src/main/java/spring/springboot/SpringbootDemoApplication.java
@@ -1,8 +1,13 @@
 package spring.springboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
 
 /**
  * @author chris
@@ -10,6 +15,23 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  * 1.不是以maven项目运行,add as maven
  * 2.运行起来报错 Unable to start ServletWebServerApplicationContext due to missing ServletWebServerFactory bean
  * 因为springboot2.0+需要tomcat8.5+版本
+ *
+ * 1.{@link SpringBootConfiguration}={@link Configuration}
+ *
+ * 2.{@link ComponentScan}
+ *
+ * 3.{@link EnableAutoConfiguration}
+ *
+ * 3a
+ * {@link AutoConfigurationPackage}
+ * {@link AutoConfigurationPackages.Registrar#registerBeanDefinitions}
+ * 注册一个BeanDefinition来存储 base package 信息
+ *
+ *
+ * 3b
+ * {@link AutoConfigurationImportSelector}
+ * {@link AutoConfigurationMetadataLoader#loadMetadata(ClassLoader, String)}
+ * 加载META-INF/spring-autoconfigure-metadata.properties中变量
  */
 @SpringBootApplication
 public class SpringbootDemoApplication extends SpringBootServletInitializer {
