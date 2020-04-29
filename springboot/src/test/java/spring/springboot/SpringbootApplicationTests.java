fa4c4a8fc484aae21a86b96a2f89f0369109c897
chris <chrishuhyc@gmail.com>
2020-04-29 16:48:56 +0800

    springboot mapper.mapperscan 源码分析

--- springboot/src/test/java/spring/springboot/SpringbootApplicationTests.java
+++ springboot/src/test/java/spring/springboot/SpringbootApplicationTests.java
@@ -16,6 +16,9 @@ import spring.springboot.pojo.Person;
 import org.mybatis.spring.boot.autoconfigure.*;
 import org.mybatis.spring.mapper.*;
 import org.apache.ibatis.annotations.*;
+import org.mybatis.spring.annotation.*;
+import org.springframework.context.annotation.*;
+import org.mybatis.spring.boot.autoconfigure.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -84,9 +87,22 @@ class SpringbootApplicationTests {
      * mybatis-spring-boot-starter包含mybatis-spring-boot-autoconfigure
      * mybatis-spring-boot-autoconfigure中定义了自定义注解类{@link MybatisAutoConfiguration}
      *
-     * 1.@Import(AutoConfiguredMapperScannerRegistrar.class),其中会注入bean{@link MapperScannerConfigurer}
+     * 1.{@link MybatisAutoConfiguration.MapperScannerRegistrarNotFoundConfiguration},如果没有
+     * @ConditionalOnMissingBean({ MapperFactoryBean.class, MapperScannerConfigurer.class })
+     * 则会以springboot的相同包名作为扫描包，
+     * @Import({@link MybatisAutoConfiguration.AutoConfiguredMapperScannerRegistrar#registerBeanDefinitions}),其中会注入bean{@link MapperScannerConfigurer}
      * 而{@link MapperScannerConfigurer#postProcessBeanDefinitionRegistry}会扫描所有{@link Mapper}
      *
+     * 2.{@link MapperScan}会Import{@link MapperScannerRegistrar},{@link MapperScannerRegistrar#registerBeanDefinitions}剩余操作同流程1
+     *
+     * 3.{@link ClassPathMapperScanner#registerFilters}确定候补candidate bean
+     *  {@link ClassPathScanningCandidateComponentProvider#scanCandidateComponents}
+     *
+     * 总结:两种自动注入mapper接口方式
+     * a.{@link Mapper}会过滤所有实现了此注解的接口
+     * b.{@link MapperScan}会过滤指定包名下的所有接口
+     * c.如果存在{@link MapperScan}则不会再启用{@link Mapper}
+     * 判断逻辑在{@link MybatisAutoConfiguration.MapperScannerRegistrarNotFoundConfiguration}
      */
     @Test
     public void testMybatis() {
