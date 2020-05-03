44f417b7fe714ff2afa61ca94c2a8b3d8d1e0197
chrishuhyc <chrishuhyc@gmail.com>
2020-05-03 16:53:24 +0800

    springboot data-redis

--- springboot/src/main/java/spring/springboot/pojo/RedisPojo.java
+++ springboot/src/main/java/spring/springboot/pojo/RedisPojo.java
@@ -1,4 +1,22 @@
 package spring.springboot.pojo;
 
+import lombok.Data;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+import javax.persistence.Id;
+
+/**
+ * @author chris
+ */
+@Data
+@RedisHash("persons")
 public class RedisPojo {
+    @Id
+    private String id;
+    @Indexed
+    private String firstName;
+    @Indexed
+    private String lastName;
+    private RedisAddress address;
 }
