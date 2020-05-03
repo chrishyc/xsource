e04fd81e6087831c95e0c03720ce22490313e851
chrishuhyc <chrishuhyc@gmail.com>
2020-05-03 17:10:41 +0800

    springboot thymeleaf

--- springboot/src/main/java/spring/springboot/controller/DemoController.java
+++ springboot/src/main/java/spring/springboot/controller/DemoController.java
@@ -1,15 +1,24 @@
 package spring.springboot.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Calendar;
 
 /**
  * @author chris
  */
 @RestController
 public class DemoController {
-    @RequestMapping(value = "/springboot",produces = "application/json; charset=utf-8")
+    @RequestMapping(value = "/springboot", produces = "application/json; charset=utf-8")
     public String sayHello() {
         return "hello spring Boot,---111222221--qqq==jjj-11111-";
+    }
+    
+    @RequestMapping("/toLoginPage")
+    public String toLoginPage(Model model) {
+        model.addAttribute("currentYear", Calendar.getInstance().get(Calendar.YEAR));
+        return "login";
     }
 }
