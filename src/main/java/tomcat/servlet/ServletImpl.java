4e33f985fe9fd1dbc85fb69fdb08ba9400d1132b
chrishuhyc <chrishuhyc@gmail.com>
2020-05-05 17:29:52 +0800

    tomcat demo

---
+++ src/main/java/tomcat/servlet/ServletImpl.java
@@ -0,0 +1,47 @@
+package tomcat.servlet;
+
+import tomcat.utils.HttpProtocolUtil;
+
+import java.io.IOException;
+
+/**
+ * @author chris
+ */
+public class ServletImpl extends HttpServlet {
+    @Override
+    public void doGet(Request request, Response response) {
+
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String content = "<h1>Servlet get</h1>";
+        try {
+            response.output((HttpProtocolUtil.getHttpHeader200(content.getBytes().length) + content));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void doPost(Request request, Response response) {
+        String content = "<h1>Servlet post</h1>";
+        try {
+            response.output((HttpProtocolUtil.getHttpHeader200(content.getBytes().length) + content));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void init() throws Exception {
+
+    }
+
+    @Override
+    public void destory() throws Exception {
+
+    }
+}
