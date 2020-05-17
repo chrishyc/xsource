ca78466b2f7d80fa86ab991af1dc4a8dd402884d
chris <chrishuhyc@gmail.com>
2020-05-17 19:50:59 +0800

    distribute job,redis session

--- src/main/resources/db/test.sql
+++ src/main/resources/db/test.sql
@@ -27,3 +27,20 @@ CREATE TABLE `DISTRIBUTE_ID`
 insert into DISTRIBUTE_ID(createtime)
 values (NOW());
 select LAST_INSERT_ID();
+
+DROP TABLE IF EXISTS `resume`;
+
+CREATE TABLE `resume`
+(
+    `id`        bigint(20) NOT NULL AUTO_INCREMENT,
+    `name`      varchar(255) DEFAULT NULL,
+    `sex`       varchar(255) DEFAULT NULL,
+    `phone`     varchar(255) DEFAULT NULL,
+    `address`   varchar(255) DEFAULT NULL,
+    `education` varchar(255) DEFAULT NULL,
+    `state`     varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1001
+  DEFAULT CHARSET = utf8;
+SET FOREIGN_KEY_CHECKS = 1;
