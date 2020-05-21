d5ee16a0d219fcc527c55373a196b6a95f4c08ee
chris <chrishuhyc@gmail.com>
2020-05-21 19:57:55 +0800

    spring.validation.hibernate.validator

--- springboot/src/main/java/spring/springboot/redissession/controller/LoginController.java
+++ springboot/src/main/java/spring/springboot/redissession/controller/LoginController.java
@@ -1,18 +1,39 @@
 package spring.springboot.redissession.controller;
 
+import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import spring.springboot.pojo.ValidPojo;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.ConstraintValidatorContext;
 import javax.validation.Valid;
-
+import javax.validation.Constraint;
+import org.springframework.web.method.annotation.*;
+import org.hibernate.validator.internal.metadata.descriptor.*;
+import org.hibernate.validator.internal.constraintvalidators.bv.number.bound.*;
+import org.hibernate.validator.internal.engine.*;
 @Controller
 @RequestMapping("login")
 public class LoginController {
     
     /**
-     * {@link org.hibernate.validator.internal.metadata.core.ConstraintHelper#ConstraintHelper()}
+     * {@link Valid}注解类生成中心,{@link org.hibernate.validator.internal.metadata.core.ConstraintHelper#ConstraintHelper()}
+     * {@link Constraint}:Marks an annotation as being a Bean Validation constraint, A given constraint annotation must be annotated
+     *
+     * 主要逻辑
+     * {@link ModelAttributeMethodProcessor#resolveArgument}
+     * 绑定{@link ModelAttributeMethodProcessor#bindRequestParameters}
+     * 校验{@link ModelAttributeMethodProcessor#validateIfApplicable}
+     * 抛异常{@link ModelAttributeMethodProcessor#isBindExceptionRequired(MethodParameter)}
+     *
+     * 校验
+     * {@link ConstraintDescriptorImpl#parseComposingConstraints}
+     * {@link AbstractMaxValidator#isValid(Object, ConstraintValidatorContext)}
+     * {@link ValidationContext#addConstraintFailures}
+     *
+     * 抛异常
+     * {@link }
      * @param user
      * @return
      */
