1777b72e311b7f12277ae3a80ec585a08a41a83a
chrishuhyc <chrishuhyc@gmail.com>
2020-05-24 22:06:20 +0800

    java.rmi

---
+++ client/src/main/java/stub/IHelloService.java
@@ -0,0 +1,17 @@
+package stub;
+
+
+import pojo.User;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * 远程服务对象接口必须要集成Remote接口的,同时方法必须抛出throws RemoteException
+ */
+public interface IHelloService extends Remote {
+
+    public String sayHello(User user)throws RemoteException;
+
+
+}
