20998083ebbc5c528622663848b58b39054409c6
chrishuhyc <chrishuhyc@gmail.com>
2020-06-01 16:05:53 +0800

    java.serializable

---
+++ java/src/main/java/serialization/serializable/User.java
@@ -0,0 +1,18 @@
+package serialization.serializable;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class User implements Serializable {
+    private String userName;
+    private String password;
+    private String sex;
+    
+    public User(String userName, String password, String sex) {
+        this.userName = userName;
+        this.password = password;
+        this.sex = sex;
+    }
+}
