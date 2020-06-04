21a10a908cc732d937d7e54455212d1dfd5e4d79
chris <chrishuhyc@gmail.com>
2020-06-04 20:05:50 +0800

    PersistenceContext.nosuchmethoderror

--- src/main/java/tx/propagation/Sample.java
+++ src/main/java/tx/propagation/Sample.java
@@ -1,20 +1,11 @@
 package tx.propagation;
 
+import org.junit.Test;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
  * MySQL的官方文档中有明确的说明不支持嵌套事务
- *
- * 在上面的1，将开起新事务A，2的时候会插入数据，此时事务A挂起，没有commit，3的时候，
- * 使用PROPAGATION_NESTED传播，将在3点的时候新建一个savepoint保存2插入的数据，不提交。
- *
- *
- * 如果methodB出现异常，将回滚4的操作，不影响2的操作，同时可以处理后面的5,6逻辑，最后一起commit: 2,5,6
- * 如果methodB没有出现异常，那么将一起commit: 2,4,6。
- * 假如methodB使用的PROPAGATION_REQUIRES_NEW，那么B异常，会commit: 2,5,6，和NESTED一致，如果methodB
- * 没有出现异常，那么会先commit4，再commit:6，那么事务将分离开，不能保持一致，假如执行6报错，2和6将回滚，
- * 而4却没有被回滚，不能达到预期效果。
  *
  * 1.Propagation.REQUIRED 扁平事务。在这种事务中，所有操作都处于同一层次。主要缺点是不能提交或回滚事务的某一部
  *
@@ -37,6 +28,11 @@ public class Sample {
             }
             updateData(); //6
         }
+        
+        @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+        void required(){
+        
+        }
     }
 
     class ServiceB {
@@ -53,5 +49,10 @@ public class Sample {
 
     private void insertData() {
 
+    }
+    
+    @Test
+    public void testRequired(){
+    
     }
 }
