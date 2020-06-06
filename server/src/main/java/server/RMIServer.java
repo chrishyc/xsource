c246164fb22ef5bf6152892f74b22add1744f2f5
chrishuhyc <chrishuhyc@gmail.com>
2020-06-06 22:56:28 +0800

    rmi核心类

--- server/src/main/java/server/RMIServer.java
+++ server/src/main/java/server/RMIServer.java
@@ -5,10 +5,39 @@ import stub.HelloServiceImpl;
 import stub.IHelloService;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
+import java.rmi.Remote;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.server.RemoteCall;
 
+import sun.rmi.registry.*;
+import sun.rmi.server.*;
+import sun.rmi.transport.tcp.*;
+/**
+ * 调试rpc:
+ * 1.查看状态机watch -n 0.5 -d 'netstat -ant | grep -E "9898|63039|63121"'
+ * 2.wireShake查看端口号tcp.port == 9898||tcp.port==63039||tcp.port==63121
+ * 3.debug
+ *
+ * 1.客户端_stub,{@link UnicastRef#invoke(Remote, Method, Object[], long)}
+ * 2.服务端_skeleton,{@link UnicastServerRef#dispatch(Remote, RemoteCall)}
+ * 3.注册中心:
+ * 客户端:RegistryImpl_Stub(send,port:63215)
+ * 服务端:RegistryImpl_Skel(accept,port:9898),{@link RegistryImpl_Skel#dispatch(Remote, RemoteCall, int, long)}
+ * 原服务:RegistryImpl
+ *
+ * 底层socket
+ * {@link TCPEndpoint}
+ *
+ * {@link TCPChannel#newConnection()}
+ * {@link TCPConnection#getInputStream()}
+ * {@link TCPConnection#getOutputStream()}
+ *
+ * {@link TCPTransport#listen()}
+ *
+ */
 public class RMIServer {
     
     public static void main(String[] args) throws IOException, AlreadyBoundException {
