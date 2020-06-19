3176b2f0ed0c48a2abb8d5d9924446496de66e94
chrishuhyc <chrishuhyc@gmail.com>
2020-06-19 20:08:23 +0800

    spring.ApplicationListener

---
+++ src/main/java/demo/spring/eventListener/MyApplicationListener.java
@@ -0,0 +1,16 @@
+package demo.spring.eventListener;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+public class MyApplicationListener implements ApplicationListener {
+    
+    public MyApplicationListener() {
+        System.out.println("MyApplicationListener start------------------------------");
+    }
+    
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        System.out.println(event == null ? "event" : event.toString());
+    }
+}
