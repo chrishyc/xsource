b59ff802533fa4d7cf0880c9751750f8bdbad8b1
chrishuhyc <chrishuhyc@gmail.com>
2020-06-19 20:00:21 +0800

    spring.ApplicationListener

--- src/main/java/ioc/Sample.java
+++ src/main/java/ioc/Sample.java
@@ -8,7 +8,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
-import org.springframework.core.type.filter.TypeFilter;
 
 public class Sample {
     @Test
