efec9cfb027e0d0c7c3727cf62646867e36294a7
chris <chrishuhyc@gmail.com>
2020-06-28 17:47:09 +0800

    java.jmx

---
+++ java/src/main/java/jmx/Book.java
@@ -0,0 +1,19 @@
+package jmx;
+
+public class Book {
+    public String name;
+    public double price;
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ java/src/main/java/jmx/HelloMX.java
@@ -0,0 +1,41 @@
+package jmx;
+
+public class HelloMX implements HelloMXBean{
+    public void sayHello() {
+        System.out.println("hello, world");
+    }
+    
+    public int add(int x, int y) {
+        return x + y;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public int getCacheSize() {
+        return this.cacheSize;
+    }
+    
+    public synchronized void setCacheSize(int size) {
+        
+        this.cacheSize = size;
+        System.out.println("Cache size now " + this.cacheSize);
+    }
+    
+    @Override
+    public Book getBook() {
+        return book;
+    }
+    
+    @Override
+    public void addBook(Book book) {
+        this.book = book;
+    }
+    
+    private final String name = "Reginald";
+    private int cacheSize = DEFAULT_CACHE_SIZE;
+    private static final int
+            DEFAULT_CACHE_SIZE = 200;
+    private Book book;
+}
