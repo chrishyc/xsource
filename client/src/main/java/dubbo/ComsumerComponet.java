07ddadbac74b5e86121c85c2f91c5be0b7dedfa7
chrishuhyc <chrishuhyc@gmail.com>
2020-07-04 20:30:26 +0800

    dubbo.annotation.demo

---
+++ client/src/main/java/dubbo/ComsumerComponet.java
@@ -0,0 +1,15 @@
+package dubbo;
+
+import demo.HelloService;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComsumerComponet {
+    @Reference
+    private HelloService helloService;
+    public String  sayHello(String name){
+        return  helloService.sayHi(name);
+    }
+
+}
