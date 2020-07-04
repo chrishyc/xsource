9e66e1c737e255b13f1003727a1d5c921300ae57
chris <chrishuhyc@gmail.com>
2020-07-04 20:30:26 +0800

    dubbo.annotation.demo

---
+++ server/src/main/java/dubbo/HelloServiceImpl.java
@@ -0,0 +1,17 @@
+package dubbo;
+
+import demo.HelloService;
+import org.apache.dubbo.config.annotation.Service;
+
+@Service
+public class HelloServiceImpl   implements HelloService {
+    @Override
+    public String sayHi(String name) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "hello:"+name;
+    }
+}
