229e34f0330ef9ae1c54667c923c13a8874427b4
chris <chrishuhyc@gmail.com>
2020-07-08 23:04:25 +0800

    dubbo.Namespace.handler

--- server/src/main/java/dubbo/spi/DubboAdaptiveMain.java
+++ server/src/main/java/dubbo/spi/DubboAdaptiveMain.java
@@ -5,9 +5,9 @@ import org.apache.dubbo.common.extension.ExtensionLoader;
 
 public class DubboAdaptiveMain {
     public static void main(String[] args) {
-        URL   url  = URL.valueOf("1://localhost/1?hello.service=dog");
-        HelloService  adaptiveExtension = ExtensionLoader.getExtensionLoader(HelloService.class).getAdaptiveExtension();
-        String  msg = adaptiveExtension.sayHello(url);
+        URL url = URL.valueOf("1://localhost/1?hello.service=dog");
+        HelloService adaptiveExtension = ExtensionLoader.getExtensionLoader(HelloService.class).getAdaptiveExtension();
+        String msg = adaptiveExtension.sayHello(url);
         System.out.println(msg);
     }
 }
