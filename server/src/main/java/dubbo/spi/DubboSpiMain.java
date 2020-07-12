96a89a9225169131df76138f813f5b111d9f32b1
chris <chrishuhyc@gmail.com>
2020-07-12 20:40:43 +0800

    dubbo.ExtensionLoader

--- server/src/main/java/dubbo/spi/DubboSpiMain.java
+++ server/src/main/java/dubbo/spi/DubboSpiMain.java
@@ -3,7 +3,18 @@ package dubbo.spi;
 import org.apache.dubbo.common.extension.ExtensionLoader;
 
 import java.util.Set;
-
+import org.apache.dubbo.common.extension.factory.*;
+import org.apache.dubbo.common.extension.*;
+import org.apache.dubbo.common.extension.Adaptive;
+/**
+ * 1.ExtensionLoader用于spi加载扩展类
+ * 2.首先通过spi加载org.apache.dubbo.common.extension.ExtensionFactory.
+ * 3.通过spi加载指定接口同名的META-INF/interface
+ * 4.每个接口对应一个{@link ExtensionLoader},包含spi文件中的所有类实现
+ * 5.objectFactory对应{@link AdaptiveExtensionFactory}
+ * 6.每个{@link ExtensionLoader}都有cachedDefaultName,对应spi文件中有{@link SPI}的{@link SPI#value()}
+ * 7.{@link Adaptive}
+ */
 public class DubboSpiMain {
     public static void main(String[] args) {
         // 获取扩展加载器
