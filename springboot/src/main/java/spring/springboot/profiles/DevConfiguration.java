cf26c4ecd63c9f37327e1fd1ef2e2a0798c89ff2
chris <chrishuhyc@gmail.com>
2020-08-17 12:53:04 +0800

    springboot.profile

---
+++ springboot/src/main/java/spring/springboot/profiles/DevConfiguration.java
@@ -0,0 +1,14 @@
+package spring.springboot.profiles;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Profile("dev")
+@PropertySource("classpath:application-dev.properties")
+@ConfigurationProperties(prefix = "chris")
+public class DevConfiguration {
+    private String name;
+}
