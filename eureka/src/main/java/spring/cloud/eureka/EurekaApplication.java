05fade7b387cfe1b2af1eaef695c99f78c5faccb
chris <chrishuhyc@gmail.com>
2020-08-20 10:02:43 +0800

    springcloud.eureka.demo

---
+++ eureka/src/main/java/spring/cloud/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package spring.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author chris
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
