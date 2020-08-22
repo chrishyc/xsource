393d7514f0dd09dafd069306f9b3e143a7c1fab7
chris <chrishuhyc@gmail.com>
2020-08-22 21:39:34 +0800

    springcloud.eureka

--- consumer/src/main/java/spring/cloud/consumer/ConsumerApplication.java
+++ consumer/src/main/java/spring/cloud/consumer/ConsumerApplication.java
@@ -3,13 +3,19 @@ package spring.cloud.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ConsumerApplication {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
     }
-
+    
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 }
---
+++ consumer/src/main/java/spring/cloud/consumer/controller/AutodeliverController.java
@@ -0,0 +1,37 @@
+package spring.cloud.consumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/autodeliver")
+public class AutodeliverController {
+    
+    @Autowired
+    private RestTemplate restTemplate;
+    
+    @Autowired
+    private DiscoveryClient discoveryClient;
+    
+    @GetMapping("/checkState/{userId}")
+    public Integer findResumeOpenState(@PathVariable Long userId) {
+        List<ServiceInstance> instances = discoveryClient.getInstances("eureka-provider");
+        // 2、如果有多个实例，选择一个使用(负载均衡的过程)
+        ServiceInstance serviceInstance = instances.get(0);
+        // 3、从元数据信息获取host port
+        String host = serviceInstance.getHost();
+        int port = serviceInstance.getPort();
+        String url = "http://" + host + ":" + port + "/resume/openstate/" + userId;
+        System.out.println("===============>>>从EurekaServer集群获取服务实例拼接的url：" + url);
+        Integer forObject = restTemplate.getForObject(url, Integer.class);
+        return forObject;
+    }
+}
