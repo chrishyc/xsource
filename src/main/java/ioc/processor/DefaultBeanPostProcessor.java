e949a86962f66bf891f34a15f29d397f5b54016e
chris <chrishuhyc@gmail.com>
2020-08-24 22:42:43 +0800

    spring.candidate.@annotation

--- src/main/java/ioc/processor/DefaultBeanPostProcessor.java
+++ src/main/java/ioc/processor/DefaultBeanPostProcessor.java
@@ -2,9 +2,11 @@ package ioc.processor;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 @Component
+@ComponentScan(basePackages = "")
 public class DefaultBeanPostProcessor implements BeanPostProcessor {
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
@@ -13,7 +15,7 @@ public class DefaultBeanPostProcessor implements BeanPostProcessor {
         System.out.println("BeanPostProcessor 实现类 postProcessBeforeInitialization 方法被调用中......");
         return bean;
     }
-
+    
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         if ("accountDao".equals(beanName)) {
--- src/main/java/resolvabletype/Sample.java
+++ src/main/java/resolvabletype/Sample.java
@@ -1,4 +1,4 @@
-package configuration;
+package resolvabletype;
 
 import org.junit.Test;
 import org.springframework.core.ResolvableType;
