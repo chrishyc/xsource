14558cd1b506a09eb8a1c4033d07ab3b85253757
chris <chrishuhyc@gmail.com>
2020-09-12 21:39:37 +0800

    spring.restTemplate

---
+++ src/main/java/resttemplate/MyServiceRequestWrapper.java
@@ -0,0 +1,25 @@
+package resttemplate;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.support.HttpRequestWrapper;
+
+import java.net.URI;
+
+public class MyServiceRequestWrapper<U, V> extends HttpRequestWrapper {
+    private final U instance;
+    
+    private final V loadBalancer;
+    
+    public MyServiceRequestWrapper(HttpRequest request, U instance,
+                                   V loadBalancer) {
+        super(request);
+        this.instance = instance;
+        this.loadBalancer = loadBalancer;
+    }
+    
+    @Override
+    public URI getURI() {
+        System.out.println("instance:" + instance + ",loadBalancer:" + loadBalancer);
+        return getRequest().getURI();
+    }
+}
