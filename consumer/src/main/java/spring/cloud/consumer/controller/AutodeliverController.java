3d4243bae085ababa33efcb283b410cb6d044b0b
chris <chrishuhyc@gmail.com>
2020-09-13 21:34:10 +0800

    springcloud.netflix.health.dashboard

--- consumer/src/main/java/spring/cloud/consumer/controller/AutodeliverController.java
+++ consumer/src/main/java/spring/cloud/consumer/controller/AutodeliverController.java
@@ -1,5 +1,8 @@
 package spring.cloud.consumer.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -9,10 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
+@DefaultProperties(defaultFallback = "defaultFallback")
 @RestController
 @RequestMapping("/autodeliver")
 public class AutodeliverController {
@@ -36,12 +38,72 @@ public class AutodeliverController {
         Integer forObject = restTemplate.getForObject(url, Integer.class);
         return forObject;
     }
-    
+    @HystrixCommand
     @GetMapping("/checkState1/{userId}")
     public Integer findResumeOpenState1(@PathVariable Long userId) {
         // 使用ribbon不需要我们自己获取服务实例然后选择一个那么去访问了（自己的负载均衡）
         String url = "http://eureka-provider/resume/openstate/" + userId;  // 指定服务名
         Integer forObject = restTemplate.getForObject(url, Integer.class);
         return forObject;
+    }
+    
+    @HystrixCommand(
+            // 线程池标识，要保持唯一，不唯一的话就共用了
+            threadPoolKey = "findResumeOpenStateTimeout",
+            // 线程池细节属性配置
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "1"), // 线程数
+                    @HystrixProperty(name = "maxQueueSize", value = "20") // 等待队列长度
+            },
+            // commandProperties熔断的一些细节属性配置
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
+            })
+    @GetMapping("/checkStateTimeout/{userId}")
+    public Integer findResumeOpenStateTimeout(@PathVariable Long userId) {
+        // 使用ribbon不需要我们自己获取服务实例然后选择一个那么去访问了（自己的负载均衡）
+        String url = "http://eureka-provider/resume/openstate/" + userId;  // 指定服务名
+        Integer forObject = restTemplate.getForObject(url, Integer.class);
+        return forObject;
+    }
+    
+    @GetMapping("/checkStateTimeoutFallback/{userId}")
+    @HystrixCommand(
+            // 线程池标识，要保持唯一，不唯一的话就共用了
+            threadPoolKey = "findResumeOpenStateTimeoutFallback",
+            // 线程池细节属性配置
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "2"), // 线程数
+                    @HystrixProperty(name = "maxQueueSize", value = "20") // 等待队列长度
+            },
+            // commandProperties熔断的一些细节属性配置
+            commandProperties = {
+                    // 每一个属性都是一个HystrixProperty
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+                    // hystrix高级配置，定制工作过程细节
+                    // 统计时间窗口定义
+                    @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "8000"),
+                    // 统计时间窗口内的最小请求数
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "2"),
+                    // 统计时间窗口内的错误数量百分比阈值
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+                    // 自我修复时的活动窗口长度
+                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "3000")
+            },
+            fallbackMethod = "myFallBack"  // 回退方法
+    )
+    public Integer findResumeOpenStateTimeoutFallback(@PathVariable Long userId) {
+        // 使用ribbon不需要我们自己获取服务实例然后选择一个那么去访问了（自己的负载均衡）
+        String url = "http://eureka-provider/resume/openstate/" + userId;  // 指定服务名
+        Integer forObject = restTemplate.getForObject(url, Integer.class);
+        return forObject;
+    }
+    
+    public Integer myFallBack(Long userId) {
+        return -123333; // 兜底数据
+    }
+    
+    public Integer defaultFallback() {
+        return 1010; // 兜底数据
     }
 }
