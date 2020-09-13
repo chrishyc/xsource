e57afb869b874f216332d07f0e3d663065fc826a
chrishuhyc <chrishuhyc@gmail.com>
2020-09-13 21:34:10 +0800

    springcloud.netflix.health.dashboard

--- consumer/src/main/java/spring/cloud/consumer/ConsumerApplication.java
+++ consumer/src/main/java/spring/cloud/consumer/ConsumerApplication.java
@@ -1,14 +1,21 @@
 package spring.cloud.consumer;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+
+@EnableHystrixDashboard
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableCircuitBreaker
 public class ConsumerApplication {
     
     public static void main(String[] args) {
@@ -19,5 +26,17 @@ public class ConsumerApplication {
     @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+    
+    
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new
+                HystrixMetricsStreamServlet();
+        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
--- provider/src/main/java/spring/cloud/provider/ProviderApplication.java
+++ provider/src/main/java/spring/cloud/provider/ProviderApplication.java
@@ -3,12 +3,14 @@ package spring.cloud.provider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * @author chris
  */
 @EnableEurekaClient
 @SpringBootApplication
+@EnableTurbine
 public class ProviderApplication {
 
     public static void main(String[] args) {
