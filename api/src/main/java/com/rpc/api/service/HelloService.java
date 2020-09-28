558aaad464f4ee9f8b70f41a2de3184f0a261f38
chrishuhyc <chrishuhyc@gmail.com>
2020-09-28 21:15:22 +0800

    api.pom

---
+++ api/src/main/java/com/rpc/api/service/HelloService.java
@@ -0,0 +1,11 @@
+package com.rpc.api.service;
+
+public interface HelloService {
+    public String sayHello(String name, int timeToWait);
+    
+    public String monitorMethodA();
+    
+    public String monitorMethodB();
+    
+    public String monitorMethodC();
+}
