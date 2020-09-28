16c8428a11bf0f3d1fd9cb90dbd2c3f6a2e034d8
chris <chrishuhyc@gmail.com>
2020-09-28 22:49:12 +0800

    springcloud.gateway.filter

--- gateway/src/main/java/spring/cloud/gateway/GatewayApplication.java
+++ gateway/src/main/java/spring/cloud/gateway/GatewayApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -52,4 +53,6 @@ public class GatewayApplication {
                 .route("path_route", r -> r.query("red", "gree").uri("http://ityouknow.com"))
                 .build();
     }
+    
+    
 }
