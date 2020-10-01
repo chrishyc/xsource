621f3656c32570e2b13adad155c6cc9dc6683dc5
chris <chrishuhyc@gmail.com>
2020-10-01 22:41:02 +0800

    springcloud.config

---
+++ configserver/src/main/java/springcloud/config/server/ServerApplication.java
@@ -0,0 +1,15 @@
+package springcloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
