85cfb808e3b48bf86c9940263da4fdda6fc60672
chrishuhyc <chrishuhyc@gmail.com>
2020-10-01 23:59:30 +0800

    springcloud.config

--- configclient/src/main/java/springcloud/config/configclient/ConfigclientApplication.java
+++ configclient/src/main/java/springcloud/config/configclient/ConfigclientApplication.java
@@ -1,13 +1,28 @@
 package springcloud.config.configclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ConfigclientApplication {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(ConfigclientApplication.class, args);
     }
-
+    
+    @RestController
+    public class HelloController {
+        @Value("${dev.hello}")
+        private String hello;
+        
+        @RefreshScope
+        @RequestMapping("/hello")
+        public String hello() {
+            return hello;
+        }
+    }
 }
