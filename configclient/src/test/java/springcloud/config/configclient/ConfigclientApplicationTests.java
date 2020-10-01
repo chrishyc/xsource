5ac2637c91d519b3b6ed452fd2ef6532deaec94e
chrishuhyc <chrishuhyc@gmail.com>
2020-10-01 22:41:02 +0800

    springcloud.config

---
+++ configclient/src/test/java/springcloud/config/configclient/ConfigclientApplicationTests.java
@@ -0,0 +1,13 @@
+package springcloud.config.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
