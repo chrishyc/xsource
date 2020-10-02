3ed810c19f4b25d84a5956553d8699732fef94e1
chris <chrishuhyc@gmail.com>
2020-10-02 23:23:00 +0800

    springcloud.stream

--- provider/src/main/java/spring/cloud/provider/ProviderApplication.java
+++ provider/src/main/java/spring/cloud/provider/ProviderApplication.java
@@ -2,13 +2,14 @@ package spring.cloud.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * @author chris
  */
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableTurbine
 public class ProviderApplication {
