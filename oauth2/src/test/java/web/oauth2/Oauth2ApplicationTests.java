ff04cda1424699607831c0213bb1ee1c80d58c77
chrishuhyc <chrishuhyc@gmail.com>
2020-10-19 23:22:51 +0800

    oauth2

---
+++ oauth2/src/test/java/web/oauth2/Oauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package web.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
