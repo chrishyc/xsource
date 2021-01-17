22e99bbf36a083a94c58852b15304949fb111424
chris <chrishuhyc@gmail.com>
2021-01-17 18:18:56 +0800

    java nio buffer

--- src/main/java/ioc/cycle/TestBean.java
+++ src/main/java/ioc/cycle/TestBean.java
@@ -4,10 +4,11 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
 //@Data
 //@Lazy
-//@Component
+@Component
 public class TestBean implements InitializingBean, ApplicationContextAware {
     private CycleBean cycleBean;
 
