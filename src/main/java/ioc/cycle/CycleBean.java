52db0cacdf307b9baaab0a1835feebb3217de55f
chrishuhyc <chrishuhyc@gmail.com>
2021-02-05 14:01:37 +0800

    bean cycle

--- src/main/java/ioc/cycle/CycleBean.java
+++ src/main/java/ioc/cycle/CycleBean.java
@@ -2,13 +2,27 @@ package ioc.cycle;
 
 import demo.spring.annotation.Autowired;
 import demo.spring.annotation.Component;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 @Component
-public class CycleBean {
+public class CycleBean implements InitializingBean, ApplicationContextAware {
     @Autowired
     private TestBean testBean;
 
     public void setTestBean(TestBean testBean) {
         this.testBean = testBean;
+    }
+    
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("CycleBean afterPropertiesSet...");
+    }
+    
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("CycleBean setApplicationContext...");
     }
 }
