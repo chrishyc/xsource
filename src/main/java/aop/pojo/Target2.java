3a2007cc979014995dacdb83fd99c36e224302a6
chrishuhyc <chrishuhyc@gmail.com>
2021-02-23 19:25:24 +0800

    spring.main.web-application-type添加属性

--- src/main/java/aop/pojo/Target2.java
+++ src/main/java/aop/pojo/Target2.java
@@ -1,10 +1,24 @@
 package aop.pojo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.*;
 
 @Component
 public class Target2 {
+    
+    @Autowired
+    private Target3 target3;
+    
     public void transfer() {
-        throw new RuntimeException("no error");
+//        throw new RuntimeException("no error");
+        Map<Integer, List<Integer>> out = new HashMap<>();
+        Iterator<Map.Entry<Integer, List<Integer>>> it = out.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<Integer, List<Integer>> entry = it.next();
+        }
+        Queue<int[]> minQ = new PriorityQueue<>(Comparator.comparingInt(o -> o[0]));
     }
+    
 }
