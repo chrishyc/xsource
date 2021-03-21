10b626f3bc235881a5e51849d774a324640f400a
chris <chrishuhyc@gmail.com>
2021-03-21 19:36:37 +0800

    dict reorder

---
+++ cpu/src/main/java/reorder/DCL.java
@@ -0,0 +1,27 @@
+package reorder;
+
+public class DCL {
+    int a = 8;
+    /**
+     * volatile必须要,防止new的三个指令重排
+     *
+     */
+    volatile static DCL dcl;
+    
+    public static DCL getInstance() {
+        if (dcl == null) {
+            synchronized (DCL.class) {
+                if (dcl == null) {
+                    dcl = new DCL();
+                }
+            }
+        }
+        return dcl;
+    }
+    
+    public static void main(String[] args) {
+        while (new DCL().a!=0){
+        }
+        System.err.println("指令重排序了");
+    }
+}
