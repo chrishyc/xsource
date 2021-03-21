43576d74b067de8be504b3ea7fcdab01933b39a6
chris <chrishuhyc@gmail.com>
2021-03-21 18:13:04 +0800

    cache 一致性

--- byteclass/src/main/java/jdk/ReflectTest.java
+++ byteclass/src/main/java/jdk/ReflectTest.java
@@ -6,8 +6,8 @@ public class ReflectTest {
     public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
         //注意一定要返回接口，不能返回实现类否则会报错
         System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
-        water water = (water) DynamicAgent.agent(water.class, new Cool());
-        Fruit fruit = (Fruit) DynamicAgent.agent(Fruit.class, new Apple());
+        jdk.water water = (jdk.water) jdk.DynamicAgent.agent(jdk.water.class, new jdk.Cool());
+        jdk.Fruit fruit = (jdk.Fruit) jdk.DynamicAgent.agent(jdk.Fruit.class, new jdk.Apple());
         water.drink();
         fruit.show();
     }
