af4b5cde862b241a6e90ac352861432672aa306b
chrishuhyc <chrishuhyc@gmail.com>
2021-03-22 22:52:35 +0800

    thread fiber

---
+++ java/src/main/java/fiber/Fiber2Test.java
@@ -0,0 +1,64 @@
+package fiber;
+
+import co.paralleluniverse.fibers.Fiber;
+import co.paralleluniverse.fibers.SuspendExecution;
+import co.paralleluniverse.strands.SuspendableRunnable;
+
+import java.util.concurrent.ExecutionException;
+
+public class Fiber2Test {
+    public static void main(String[] args) throws Exception {
+    
+        long start = System.currentTimeMillis();
+        Runnable r = () -> {
+            try {
+                fiberCalc();
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+    
+        int size = 2;
+    
+        Thread[] threads = new Thread[size];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(r);
+        }
+    
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].start();
+        }
+    
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].join();
+        }
+    
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+    }
+    
+    static void fiberCalc() throws ExecutionException, InterruptedException {
+        int size = 5000;
+    
+        Fiber<Void>[] fibers = new Fiber[size];
+    
+        for (int i = 0; i < fibers.length; i++) {
+            fibers[i] = new Fiber<Void>((SuspendableRunnable) () -> calc());
+        }
+    
+        for (int i = 0; i < fibers.length; i++) {
+            fibers[i].start();
+        }
+    
+        for (int i = 0; i < fibers.length; i++) {
+            fibers[i].join();
+        }
+    }
+    
+    static void calc() {
+        int result = 0;
+        for (int m = 0; m < 10000; m++) {
+            for (int i = 0; i < 200; i++) result += i;
+        }
+    }
+}
