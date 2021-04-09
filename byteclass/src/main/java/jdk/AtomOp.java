947e0b191bce2cb2be81241e40a5649031ab5993
chrishuhyc <chrishuhyc@gmail.com>
2021-04-09 09:51:23 +0800

    cpu asm

---
+++ byteclass/src/main/java/jdk/AtomOp.java
@@ -0,0 +1,9 @@
+package jdk;
+
+public class AtomOp {
+    
+    public static void main(String[] args) {
+        int i = 0;
+        i++;
+    }
+}
---
+++ cpu/src/main/java/cacheline/MultiCpuCache.java
@@ -0,0 +1,30 @@
+package cacheline;
+
+public class MultiCpuCache {
+    
+    private Long total = 0L;
+    
+    //循环一万次相加
+    private void add() {
+        for (int i = 0; i < 10000; i++) {
+            total += 1;
+        }
+    }
+    
+    //开启两个线程相加
+    public static void main(String[] args) throws InterruptedException {
+        MultiCpuCache thread = new MultiCpuCache();
+        //创建两个线程
+        Thread thread1 = new Thread(thread::add);
+        Thread thread2 = new Thread(thread::add);
+        
+        //启动线程
+        thread1.start();
+        thread2.start();
+        
+        //阻塞主线程
+        thread1.join();
+        thread2.join();
+        System.out.println(thread.total);
+    }
+}
