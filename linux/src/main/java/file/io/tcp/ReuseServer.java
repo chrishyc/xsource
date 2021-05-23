595356c8846342530d72472705d14616f8a5e4a5
chris <chrishuhyc@gmail.com>
2021-05-23 20:02:58 +0800

    socket reuse

--- linux/src/main/java/file/io/tcp/ReuseServer.java
+++ linux/src/main/java/file/io/tcp/ReuseServer.java
@@ -3,6 +3,7 @@ package file.io.tcp;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,7 +19,9 @@ public class ReuseServer {
     
     public ReuseServer(int port) throws IOException {
         // 1. 创建一个 ServerSocket 并监听端口 port
-        mServerSocket = new ServerSocket(port);
+        mServerSocket = new ServerSocket();
+        mServerSocket.setReuseAddress(true);
+        mServerSocket.bind(new InetSocketAddress(9877));
     }
     
     public void run() throws IOException {
@@ -49,7 +52,7 @@ public class ReuseServer {
     
     public static void main(String[] argv) {
         try {
-            ReuseServer server = new ReuseServer(9878);
+            ReuseServer server = new ReuseServer(9877);
             server.run();
         } catch (IOException e) {
             e.printStackTrace();
