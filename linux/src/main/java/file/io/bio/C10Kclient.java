ec7b7ab45b284ff09ba0c3dd391d55fd101cb2d2
chrishuhyc <chrishuhyc@gmail.com>
2021-05-31 10:15:09 +0800

    多路复用

--- linux/src/main/java/file/io/bio/C10Kclient.java
+++ linux/src/main/java/file/io/bio/C10Kclient.java
@@ -13,7 +13,7 @@ public class C10Kclient {
 
     public static void main(String[] args) {
         LinkedList<SocketChannel> clients = new LinkedList<>();
-        InetSocketAddress serverAddr = new InetSocketAddress("192.168.150.11", 9090);
+        InetSocketAddress serverAddr = new InetSocketAddress("localhost", 9090);
 
         //端口号的问题：65535
         //  windows
@@ -21,7 +21,7 @@ public class C10Kclient {
             try {
                 SocketChannel client1 = SocketChannel.open();
 
-                SocketChannel client2 = SocketChannel.open();
+//                SocketChannel client2 = SocketChannel.open();
 
                 /*
                 linux中你看到的连接就是：
@@ -29,15 +29,15 @@ public class C10Kclient {
                 client...port: 10508
                  */
 
-                client1.bind(new InetSocketAddress("192.168.150.1", i));
+                client1.bind(new InetSocketAddress("localhost", i));
                 //  192.168.150.1：10000   192.168.150.11：9090
                 client1.connect(serverAddr);
                 clients.add(client1);
 
-                client2.bind(new InetSocketAddress("192.168.110.100", i));
-                //  192.168.110.100：10000  192.168.150.11：9090
-                client2.connect(serverAddr);
-                clients.add(client2);
+//                client2.bind(new InetSocketAddress("192.168.110.100", i));
+//                //  192.168.110.100：10000  192.168.150.11：9090
+//                client2.connect(serverAddr);
+//                clients.add(client2);
 
             } catch (IOException e) {
                 e.printStackTrace();
