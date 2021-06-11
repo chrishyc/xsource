4450618993fe82aff8b069ce9a0dbe6666516375
chris <chrishuhyc@gmail.com>
2021-06-11 10:54:22 +0800

    netty demo

--- java/src/main/java/pattern/Sample.java
+++ java/src/main/java/pattern/Sample.java
@@ -2,16 +2,26 @@ package pattern;
 
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Sample {
+    
     @Test
-    public void testOr(){
+    public void testDatetime() {
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMM01");
+        System.out.println(format.format(new Date()));
+    }
+    
+    @Test
+    public void testOr() {
         System.out.println(".a/b".replaceAll("[./]", "_"));
     }
     
     @Test
-    public void testChar(){
-        int c=2;
-        char d= (char) (c+'0');
+    public void testChar() {
+        int c = 2;
+        char d = (char) (c + '0');
         System.out.println(d);
     }
 }
