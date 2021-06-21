2697a92056d9d87c9920f91db680368cb449abae
chrishuhyc <chrishuhyc@gmail.com>
2021-06-21 12:31:04 +0800

    权限管理模型

---
+++ auth/src/main/java/web/oauth2/controller/OauthController.java
@@ -0,0 +1,23 @@
+package web.oauth2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 参考:
+ * https://www.cnblogs.com/0201zcr/p/5131602.html
+ * https://mp.weixin.qq.com/debug/cgi-bin/sandboxinfo?action=showinfo&t=sandbox/index
+ *
+ * access_token vs refresh_token:https://www.cnblogs.com/blowing00/p/4524132.html
+ */
+@RestController
+public class OauthController {
+    
+    @RequestMapping(value = "/wx", produces = {"application/json;charset=utf-8"})
+    public String hello(HttpServletRequest request, String echostr) {
+        System.out.println(echostr);
+        return echostr;
+    }
+}
