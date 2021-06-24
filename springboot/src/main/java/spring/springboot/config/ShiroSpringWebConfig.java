f28b712ed172e260f204f5032e6484281e91e286
chrishuhyc <chrishuhyc@gmail.com>
2021-06-24 21:19:56 +0800

    shiro

---
+++ springboot/src/main/java/spring/springboot/config/ShiroSpringWebConfig.java
@@ -0,0 +1,256 @@
+package spring.springboot.config;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.mgt.DefaultSubjectDAO;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.session.mgt.DefaultSessionManager;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.subject.SubjectContext;
+import org.apache.shiro.web.filter.authc.BasicHttpAuthenticationFilter;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.mgt.DefaultWebSessionStorageEvaluator;
+import org.apache.shiro.web.mgt.DefaultWebSubjectFactory;
+import org.apache.shiro.web.servlet.OncePerRequestFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 1.引入shiro会影响其他项目的某些框架加载吗?
+ * 2.引入新shiro会影响其他api path的校验吗?
+ * 会影响,限制路径
+ * 3.多个ShiroFilterFactoryBean中的filter会互相干扰吗?会生成多个独立的FactoryShiroFilter,他们的filter互不干扰
+ * 只能使用一个ShiroFilterFactoryBean，多个无效，不会递归调用
+ * <p>
+ * 4.引入shiro后session存在哪?同一个用户会存几个session?session有缓存吗?缓存在哪?
+ * <p>
+ * 5.session应该无状态吗?
+ */
+@Configuration
+public class ShiroSpringWebConfig {
+
+//    @Bean
+//    public Filter myFilter() {
+//        return (request, response, chain) -> System.out.println("hello filter, import by bean");
+//    }
+    
+    @Bean(name = "shiroFilter")
+    public ShiroFilterFactoryBean shiroFilter() {
+        System.out.println("shiroFilter=============");
+        ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
+        Map<String, Filter> filterMap = new LinkedHashMap<>();
+        filterMap.put("myAnon", new MyFilter());
+        filterMap.put("myBasic", new BasicHttpAuthenticationFilter());
+        bean.setFilters(filterMap);
+        DefaultWebSecurityManager securityManager = securityManager();
+        bean.setSecurityManager(securityManager);
+        Map<String, String> filterMap1 = new LinkedHashMap<>();
+        
+        filterMap1.put("/list", "myBasic");
+//        filterMap1.put("/**", "myBasic");
+        
+        bean.setFilterChainDefinitionMap(filterMap1);
+        return bean;
+    }
+    
+    /**
+     * org.apache.catalina.core.ApplicationFilterChain#internalDoFilter递归时
+     * ShiroFilterFactoryBean中的filter不会调用递归方法,因此ShiroFilterFactoryBean
+     * 作为递归过程最后一个filter
+     * <p>
+     * <p>
+     * 错误,之前使用的是普通filter没有使用chain.doFilter才没有递归下去
+     * <p>
+     * FilterRegistrationBean会进行排序,ShiroFilter优先级最低
+     *
+     * @return
+     */
+    @Bean({"shiroFilter1"})
+    public ShiroFilterFactoryBean shiroFilter$1() {
+        ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
+        Map<String, Filter> filterMap = new LinkedHashMap<>();
+        filterMap.put("helloFilter", new AuthProxyFilter());
+        bean.setFilters(filterMap);
+        bean.setSecurityManager(securityManager$1());
+        Map<String, String> filterMap1 = new LinkedHashMap<>();
+        
+        filterMap1.put("/springboot", "helloFilter");
+        
+        bean.setFilterChainDefinitionMap(filterMap1);
+        return bean;
+    }
+    
+    /**
+     * 此方案无效,ShiroFilterFactoryBean中filter逻辑已结束，再次添加并没有添加到对应filter中
+     *
+     * @param context
+     * @return
+     */
+//    @Bean
+//    @ConditionalOnBean(ShiroFilterFactoryBean.class)
+//    public Object shiroFilter$2(ApplicationContext context) {
+//        System.out.println("shiroFilter$2=============");
+//        ShiroFilterFactoryBean shiroFilterFactoryBean = (ShiroFilterFactoryBean) context.getBean("&shiroFilter");
+//        shiroFilterFactoryBean.getFilters().put("helloFilter", new MyFilter$1());
+//        shiroFilterFactoryBean.getFilterChainDefinitionMap().put("/list", "helloFilter");
+//        return new Object();
+//    }
+    
+    /**
+     * 此方案有效
+     *
+     * @param context
+     * @return
+     */
+//    @Bean
+//    @ConditionalOnBean(ShiroFilterFactoryBean.class)
+//    public Object shiroFilter$3(ApplicationContext context) {
+//        System.out.println("shiroFilter$2=============");
+//        try {
+//            Class clazz = Class.forName("org.apache.shiro.spring.web.ShiroFilterFactoryBean$SpringShiroFilter");
+//            System.out.println("clazzName:" + clazz.getName());
+//            AbstractShiroFilter filter = (AbstractShiroFilter) context.getBean(clazz);
+//            PathMatchingFilterChainResolver resolver = (PathMatchingFilterChainResolver) filter.getFilterChainResolver();
+//            resolver.getFilterChainManager().getFilters().put("helloFilter", new MyFilter$1());
+//            resolver.getFilterChainManager().addToChain("/**", "helloFilter");
+//        } catch (ClassNotFoundException e) {
+//            System.out.println("error:" + e);
+//        }
+//        return new Object();
+//    }
+//
+    @Bean
+    public DefaultWebSecurityManager securityManager() {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(customRealm());
+        return securityManager;
+    }
+    
+    @Bean
+    public DefaultWebSecurityManager securityManager$1() {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(customRealm$1());
+        securityManager.setSessionManager(defaultSessionManager());
+        DefaultSubjectDAO subjectDAO = (DefaultSubjectDAO) securityManager.getSubjectDAO();
+        DefaultWebSessionStorageEvaluator webSessionStorageEvaluator = (DefaultWebSessionStorageEvaluator) subjectDAO.getSessionStorageEvaluator();
+        webSessionStorageEvaluator.setSessionStorageEnabled(false);
+        return securityManager;
+    }
+    
+    @Bean
+    public CustomRealm customRealm() {
+        return new CustomRealm();
+    }
+    
+    @Bean
+    public CustomRealm$1 customRealm$1() {
+        CustomRealm$1 customRealm$1 = new CustomRealm$1();
+        customRealm$1.setCachingEnabled(false);
+        return customRealm$1;
+    }
+    
+    @Bean
+    public DefaultSessionManager defaultSessionManager() {
+        DefaultSessionManager defaultSessionManager = new DefaultSessionManager();
+        defaultSessionManager.setSessionValidationSchedulerEnabled(false);
+        return defaultSessionManager;
+    }
+    
+    class MyFilter extends OncePerRequestFilter {
+        
+        @Override
+        protected void doFilterInternal(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+            System.out.println("==========MyFilter==========");
+            chain.doFilter(request, response);
+        }
+    }
+    
+    class MyFilter$1 extends BasicHttpAuthenticationFilter {
+        
+        @Override
+        protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
+            return false;
+        }
+        
+        
+        @Override
+        protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+            System.out.println("==========MyFilter111111==========");
+            return super.onAccessDenied(request, response);
+        }
+        
+        @Override
+        protected boolean onLoginSuccess(AuthenticationToken token, Subject subject, ServletRequest request, ServletResponse response) throws Exception {
+            subject.getSession().stop();
+            return super.onLoginSuccess(token, subject, request, response);
+        }
+    }
+    
+    public class StatelessDefaultSubjectFactory extends DefaultWebSubjectFactory {
+        public Subject createSubject(SubjectContext context) {
+            //不创建 session
+            context.setSessionCreationEnabled(false);
+            return super.createSubject(context);
+        }
+    }
+    
+    
+    public class CustomRealm extends AuthorizingRealm {
+        
+        @Override
+        protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+            return null;
+        }
+        
+        @Override
+        protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+            String username = (String) token.getPrincipal();
+            if (!"javaboy".equals(username)) {
+                throw new UnknownAccountException("账户不存在!");
+            }
+            return new SimpleAuthenticationInfo(username, "123", getName());
+        }
+    }
+    
+    public class CustomRealm$1 extends AuthorizingRealm {
+        
+        @Override
+        protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+            return null;
+        }
+        
+        @Override
+        protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+            String username = (String) token.getPrincipal();
+            if (!"chris".equals(username)) {
+                throw new UnknownAccountException("账户不存在!");
+            }
+            return new SimpleAuthenticationInfo(username, "123", getName());
+        }
+    }
+    
+    public class AuthProxyFilter implements Filter {
+        
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+            HttpServletRequest servletRequest = (HttpServletRequest) request;
+            HttpServletResponse servletResponse = (HttpServletResponse) response;
+            String username = servletRequest.getParameter("user");
+            String password = servletRequest.getParameter("password");
+            if ("/springboot".equals(servletRequest.getRequestURI()) && "chris".equals(username) && "123".equals(password)) {
+                chain.doFilter(servletRequest, servletResponse);
+                return;
+            }
+        }
+    }
+    
+}
