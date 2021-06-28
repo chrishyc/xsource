88a46a4343690e558d61631ef1484526febf717f
chrishuhyc <chrishuhyc@gmail.com>
2021-06-28 18:05:29 +0800

    shiro servlet container

--- springboot/src/main/java/spring/springboot/controller/StatusCodeController.java
+++ springboot/src/main/java/spring/springboot/controller/StatusCodeController.java
@@ -4,10 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spring.springboot.exception.ServerException;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 @RestController
 public class StatusCodeController {
     @GetMapping("/list")
-    public String getBooks() throws ServerException {
+    public String getBooks(HttpServletRequest request, HttpServletResponse response) throws ServerException {
+        HttpSession session = request.getSession();
         return "hello";
     }
 }
