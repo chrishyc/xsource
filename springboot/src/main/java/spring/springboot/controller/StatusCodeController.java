b5dbc20cc443e7d087a7980c8abe44f3d3fb5cbd
chrishuhyc <chrishuhyc@gmail.com>
2021-06-30 23:55:20 +0800

    shiro test

--- springboot/src/main/java/spring/springboot/controller/StatusCodeController.java
+++ springboot/src/main/java/spring/springboot/controller/StatusCodeController.java
@@ -1,7 +1,10 @@
 package spring.springboot.controller;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import spring.springboot.config.ShiroSpringWebConfig;
 import spring.springboot.exception.ServerException;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +16,8 @@ public class StatusCodeController {
     @GetMapping("/list")
     public String getBooks(HttpServletRequest request, HttpServletResponse response) throws ServerException {
         HttpSession session = request.getSession();
+        Subject subject = SecurityUtils.getSubject();
+        subject.login(new ShiroSpringWebConfig.MyToken());
         return "hello";
     }
 }
