404979a6bca6fe3721e73cfc09e65c20a6e972ec
chrishuhyc <chrishuhyc@gmail.com>
2021-07-01 21:29:10 +0800

    auth filter

---
+++ springboot/src/main/java/spring/springboot/filter/BasicFilter.java
@@ -0,0 +1,64 @@
+package spring.springboot.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
+import sun.misc.BASE64Decoder;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class BasicFilter implements Filter {
+    
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        int resultStatusCode = checkHTTPBasicAuthorize(request);
+        if (resultStatusCode != 0) {
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+    
+    private int checkHTTPBasicAuthorize(ServletRequest request) {
+        try {
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            String auth = httpRequest.getHeader("Authorization");
+            if ((auth != null) && (auth.length() > 6)) {
+                String HeadStr = auth.substring(0, 5).toLowerCase();
+                if (HeadStr.compareTo("basic") == 0) {
+                    auth = auth.substring(6, auth.length());
+                    String decodedAuth = getFromBASE64(auth);
+                    if (decodedAuth != null) {
+                        String[] UserArray = decodedAuth.split(":");
+                        
+                        if (UserArray != null && UserArray.length == 2) {
+                            if (UserArray[0].compareTo("chris") == 0
+                                    && UserArray[1].compareTo("123") == 0) {
+                                return 0;
+                            }
+                        }
+                    }
+                }
+            }
+            return -1;
+        } catch (Exception ex) {
+            return -2;
+        }
+        
+    }
+    
+    private String getFromBASE64(String s) {
+        if (s == null)
+            return null;
+        BASE64Decoder decoder = new BASE64Decoder();
+        try {
+            byte[] b = decoder.decodeBuffer(s);
+            return new String(b);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
