12744c4e9e6ddebd22b0a801d83a7953cdd51f7c
chrishuhyc <chrishuhyc@gmail.com>
2021-07-05 21:38:47 +0800

    visitor pattern

---
+++ system_designer/src/pattern/visitor/Demo.java
@@ -0,0 +1,21 @@
+package pattern.visitor;
+
+
+public class Demo {
+    public static void main(String[] args) {
+        Dot dot = new Dot(1, 10, 55);
+        CompoundShape compoundShape = new CompoundShape(4);
+        compoundShape.add(dot);
+        
+        CompoundShape c = new CompoundShape(5);
+        c.add(dot);
+        compoundShape.add(c);
+        
+        export(compoundShape);
+    }
+    
+    private static void export(Shape... shapes) {
+        XMLExportVisitor exportVisitor = new XMLExportVisitor();
+        System.out.println(exportVisitor.export(shapes));
+    }
+}
