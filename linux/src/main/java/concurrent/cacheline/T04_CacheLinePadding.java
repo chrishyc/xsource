bbbca1339894dfb114da3a8bfe4cd6d37ca5c385
chris <chrishuhyc@gmail.com>
2021-07-08 13:15:01 +0800

    opt module

---
+++ linux/src/main/java/concurrent/cacheline/T04_CacheLinePadding.java
@@ -0,0 +1,24 @@
+package concurrent.cacheline;
+
+public class T04_CacheLinePadding {
+    public static volatile long[] arr = new long[2];
+    
+    public static void main(String[] args) throws Exception {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10_0000_0000; i++) {
+                arr[0] = i;
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 10_0000_0000; i++) {
+                arr[1] = i;
+            }
+        });
+        final long start = System.nanoTime();
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println((System.nanoTime() - start) / 100_0000);
+    }
+}
