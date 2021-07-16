4062ce630d691d088f5c17d62c1e13f1744f556e
chris <chrishuhyc@gmail.com>
2021-07-16 11:15:27 +0800

    prometheus

--- monitor/src/main/java/prometheus/monitor/PrometheusCustomMonitor.java
+++ monitor/src/main/java/prometheus/monitor/PrometheusCustomMonitor.java
@@ -1,43 +1,84 @@
 package prometheus.monitor;
 
-import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.DistributionSummary;
-import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class PrometheusCustomMonitor {
-  /**
-   * 订单发起次数
-   */
-  private Counter orderCount;
-
-  /**
-   * 金额统计
-   */
-  private DistributionSummary amountSum;
-
-  private final MeterRegistry registry;
-
-  @Autowired
-  public PrometheusCustomMonitor(MeterRegistry registry) {
-    this.registry = registry;
-  }
-
-  @PostConstruct
-  private void init() {
-    orderCount = registry.counter("order_request_count", "order", "test-svc");
-    amountSum = registry.summary("order_amount_sum", "orderAmount", "test-svc");
-  }
-
-  public Counter getOrderCount() {
-    return orderCount;
-  }
-
-  public DistributionSummary getAmountSum() {
-    return amountSum;
-  }
+    /**
+     * 订单发起次数
+     */
+    private Counter orderCount;
+    
+    private Counter orderCount1;
+    
+    /**
+     * 金额统计
+     */
+    private DistributionSummary amountSum;
+    
+    private final MeterRegistry registry;
+    
+    @Autowired
+    public PrometheusCustomMonitor(MeterRegistry registry) {
+        this.registry = registry;
+    }
+    
+    /**
+     * id相同但打点type不同异常
+     * @Override
+     *         public String toString() {
+     *             return "MeterId{" +
+     *                     "name='" + name + '\'' +
+     *                     ", tags=" + tags +
+     *                     '}';
+     *         }
+     *
+     *         @Override
+     *         public boolean equals(@Nullable Object o) {
+     *             if (this == o) return true;
+     *             if (o == null || getClass() != o.getClass()) return false;
+     *             Meter.Id meterId = (Meter.Id) o;
+     *             return Objects.equals(name, meterId.name) && Objects.equals(tags, meterId.tags);
+     *         }
+     *
+     *         @Override
+     *         public int hashCode() {
+     *             int result = name.hashCode();
+     *             result = 31 * result + tags.hashCode();
+     *             return result;
+     *         }
+     */
+    @PostConstruct
+    private void init() {
+//        orderCount = registry.counter("order_request_count", "order", "test-svc");
+//        orderCount1 = registry.counter("order_request_count", "order", "test-svc", "name", "1111");
+//        amountSum = registry.summary("order_request_count", "orderAmount", "test-svc");
+        Metrics.counter("order_request_count","2","1").increment(1);
+//        Timer.builder("order_request_count")
+//                .publishPercentileHistogram()
+//                .register(Metrics.globalRegistry)
+//                .record(1, TimeUnit.MILLISECONDS);
+        Timer.builder("order_request_count")
+                .publishPercentileHistogram()
+                .tags("2","1")
+                .register(Metrics.globalRegistry)
+                .record(1, TimeUnit.MILLISECONDS);
+    }
+    
+    public Counter getOrderCount() {
+        return orderCount;
+    }
+    
+    public Counter getOrderCount1() {
+        return orderCount1;
+    }
+    
+    public DistributionSummary getAmountSum() {
+        return amountSum;
+    }
 }
