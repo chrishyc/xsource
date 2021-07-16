1f2b610a6d9f82ce41413590a1ee09b888eeae82
chrishuhyc <chrishuhyc@gmail.com>
2021-07-16 11:15:27 +0800

    prometheus

--- monitor/src/main/java/prometheus/controller/TestController.java
+++ monitor/src/main/java/prometheus/controller/TestController.java
@@ -1,14 +1,16 @@
 package prometheus.controller;
 
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.Timer;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import prometheus.monitor.PrometheusCustomMonitor;
-import prometheus.service.AbstractAlertService;
 
 import javax.annotation.Resource;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class TestController {
@@ -18,17 +20,17 @@ public class TestController {
   @Resource
   private PrometheusCustomMonitor monitor;
 
-  @Resource
-  private AbstractAlertService alertService;
 
   @RequestMapping("/order")
   public String order() throws Exception {
     // 统计下单次数
     monitor.getOrderCount().increment();
+    monitor.getOrderCount1().increment();
     Random random = new Random();
     int amount = random.nextInt(100);
     // 统计金额
     monitor.getAmountSum().record(amount);
+    
     return "下单成功, 金额: " + amount;
   }
 
@@ -36,9 +38,7 @@ public class TestController {
   @RequestMapping("/alert")
   public String send(@RequestBody String json) {
     System.out.println(json);
-
-    alertService.sendNotice(json);
-
+    
     return "";
   }
 }
