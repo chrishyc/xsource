f390ce3866dac4410bd41ee1623bc26f4cbd283d
chris <chrishuhyc@gmail.com>
2021-07-25 18:27:55 +0800

    copy-on-write

--- linux/src/main/java/concurrent/T25_Collection_list_CopyOnWriteList.java
+++ linux/src/main/java/concurrent/T25_Collection_list_CopyOnWriteList.java
@@ -1,12 +1,3 @@
-/**
- * дʱ�������� copy on write
- * ���̻߳����£�дʱЧ�ʵͣ���ʱЧ�ʸ�
- * �ʺ�д�ٶ���Ļ���
- *
- *
- *
- * @author ��ʿ��
- */
 package concurrent;
 
 import java.util.Arrays;
@@ -14,46 +5,44 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+/**
+ * add=lock+volatile
+ * get=volatile,保证整个读的过程一致
+ */
 public class T25_Collection_list_CopyOnWriteList {
-	public static void main(String[] args) {
-		List<String> lists =
-				//new ArrayList<>(); //�������������⣡
-				//new Vector();
-				new CopyOnWriteArrayList<>();
-		Random r = new Random();
-		Thread[] ths = new Thread[100];
-		
-		for(int i=0; i<ths.length; i++) {
-			Runnable task = new Runnable() {
-	
-				@Override
-				public void run() {
-					for(int i=0; i<1000; i++) lists.add("a" + r.nextInt(10000));
-				}
-				
-			};
-			ths[i] = new Thread(task);
-		}
-		
-		
-		runAndComputeTime(ths);
-		
-		System.out.println(lists.size());
-	}
-	
-	static void runAndComputeTime(Thread[] ths) {
-		long s1 = System.currentTimeMillis();
-		Arrays.asList(ths).forEach(t->t.start());
-		Arrays.asList(ths).forEach(t->{
-			try {
-				t.join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		});
-		long s2 = System.currentTimeMillis();
-		System.out.println(s2 - s1);
-		
-	}
+    public static void main(String[] args) {
+        List<String> lists =
+                //new ArrayList<>(); //
+                //new Vector();
+                new CopyOnWriteArrayList<>();
+        Random r = new Random();
+        Thread[] ths = new Thread[10];
+        
+        for (int i = 0; i < ths.length; i++) {
+            Runnable task = () -> {
+                for (int i1 = 0; i1 < 1000; i1++) lists.add("a" + r.nextInt(10000));
+                System.out.println(Thread.currentThread().getName() + ":" + "==========finish========");
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + ":" + lists.size());
+            };
+            ths[i] = new Thread(task);
+        }
+        
+        runAndComputeTime(ths);
+        
+        System.out.println(lists.size());
+    }
+    
+    static void runAndComputeTime(Thread[] ths) {
+        long s1 = System.currentTimeMillis();
+        Arrays.asList(ths).forEach(t -> t.start());
+        long s2 = System.currentTimeMillis();
+        System.out.println(s2 - s1);
+        
+    }
 }
 
