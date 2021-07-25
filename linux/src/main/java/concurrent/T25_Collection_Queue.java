536eeaba5833e6f9a078cc70b2a0fc928fb8db95
chrishuhyc <chrishuhyc@gmail.com>
2021-07-25 21:30:28 +0800

    collection

--- linux/src/main/java/concurrent/T25_Collection_Queue.java
+++ linux/src/main/java/concurrent/T25_Collection_Queue.java
@@ -1,7 +1,10 @@
 package concurrent;
 
+import org.junit.Test;
+
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class T25_Collection_Queue {
     public static void main(String[] args) {
@@ -11,5 +14,52 @@ public class T25_Collection_Queue {
         q.add(2);
         q.add(3);
         System.out.println(q);
+    }
+    
+    @Test
+    public void testAdd(){
+        Queue<Integer> q = new ArrayBlockingQueue<>(2);
+        q.add(0);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+    }
+    
+    @Test
+    public void testOffer(){
+        Queue<Integer> q = new ArrayBlockingQueue<>(2);
+        q.offer(0);
+        q.offer(1);
+        q.offer(2);
+        q.offer(3);
+    }
+    
+    
+    @Test
+    public void testPut() throws InterruptedException {
+        BlockingQueue<Integer> q = new ArrayBlockingQueue<>(2);
+        q.put(0);
+        q.put(1);
+        q.put(2);
+        q.put(3);
+    }
+    
+    
+    @Test
+    public void testRemove(){
+        Queue<Integer> q = new ArrayBlockingQueue<>(2);
+        q.add(0);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+    }
+    
+    @Test
+    public void testPoll(){
+        Queue<Integer> q = new ArrayBlockingQueue<>(2);
+        q.poll();
+        q.poll();
+        q.poll();
+        q.poll();
     }
 }
--- linux/src/main/java/concurrent/z_集合问题清单.md
+++ linux/src/main/java/concurrent/z_集合问题清单.md
@@ -7,3 +7,13 @@
 如果将 array 数组设定为 volitile 的， 对 volatile 变量写 happens-before 读，读线程不是能够感知到 volatile 变量的变化
 原因是，这里 volatile 的修饰的仅仅只是数组引用，数组中的元素的修改是不能保证可见性的。因此 COW 采用的是新旧两个数据容器，
 通过第 5 行代码将数组引用指向新的数组。
+
+##blocking queue
+###ArrayBlockingQueue
+![](.z_集合问题清单_images/ArrayBlockingQueue.png)
+###LinkedBlockingQueue
+![](.z_集合问题清单_images/LinkedBlockingQueue.png)
+###linux如何实现阻塞指定时间?
+###PriorityQueque
+
+##ConcurrentHashMap
