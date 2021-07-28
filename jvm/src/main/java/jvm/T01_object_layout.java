1e22df579c3de72c8c4d616c1109f778cbac77cf
chris <chrishuhyc@gmail.com>
2021-07-28 22:30:51 +0800

    volatile

---
+++ jvm/src/main/java/jvm/T01_object_layout.java
@@ -0,0 +1,11 @@
+package jvm;
+
+public class T01_object_layout {
+    public static void main(String[] args) {
+        Foo foo = new Foo();
+    }
+    
+    static class Foo {
+    
+    }
+}
--- linux/src/main/java/concurrent/T07_Volatile_cacheline.java
+++ linux/src/main/java/concurrent/T07_Volatile_cacheline.java
@@ -5,17 +5,23 @@ package concurrent;
 
 import sun.misc.Contended;
 
+/**
+ * -XX:-RestrictContended
+ *
+ * TODO 本文现象比较特殊,待进一步调研
+ */
 public class T07_Volatile_cacheline implements Runnable {
     
     static class ObjectA {
         @Contended
         private long flag = 1;
-        ;
         @Contended
         private long flag10 = 1;
     }
     
-    private volatile ObjectA a;
+    public static int i = 0;
+    
+    private /**volatile*/ ObjectA a;
     
     
     public T07_Volatile_cacheline(ObjectA a) {
@@ -25,7 +31,7 @@ public class T07_Volatile_cacheline implements Runnable {
     @Override
     public void run() {
         while (a.flag == 1 || a.flag10 == 1) {
-//            i++;
+            i++;
 //            System.out.println(a.flag);
         }
     }
@@ -37,12 +43,15 @@ public class T07_Volatile_cacheline implements Runnable {
     
     public static void main(String[] args) throws InterruptedException {
         T07_Volatile_cacheline test = new T07_Volatile_cacheline(new ObjectA());
+//        System.out.println(ClassLayout.parseInstance(new ObjectA()).toPrintable() + "\n" + "thread id:" + Thread.currentThread().getId());
         Thread t = new Thread(test);
         t.start();
         
-        Thread.sleep(1000);// 保证线程已启动
+//        Thread.sleep(1);// 保证线程已启动
+        for (int i=0;i<1000000000L;i++);
         test.stop();
         t.join();
+        System.out.println(i);
     }
     
 }
