51cc56c3e58758ad542c7b26f1badd0e15ca0d5e
chris <chrishuhyc@gmail.com>
2021-08-03 15:03:58 +0800

    BenchMark

---
+++ network/src/test/java/com/xsource/network/NetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsource.network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
