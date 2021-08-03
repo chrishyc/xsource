fe9c1773e44aaa96986cc789d7513ff73cfa8859
chrishuhyc <chrishuhyc@gmail.com>
2021-08-03 15:03:58 +0800

    BenchMark

---
+++ network/src/main/java/com/xsource/network/NetworkApplication.java
@@ -0,0 +1,13 @@
+package com.xsource.network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkApplication.class, args);
+    }
+
+}
