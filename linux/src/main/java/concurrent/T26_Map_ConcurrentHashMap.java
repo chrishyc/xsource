f3dd2773334718d3aa3cbe1ea60966eb7016b699
chris <chrishuhyc@gmail.com>
2021-08-04 16:13:03 +0800

    nginx

--- linux/src/main/java/concurrent/T26_Map_ConcurrentHashMap.java
+++ linux/src/main/java/concurrent/T26_Map_ConcurrentHashMap.java
@@ -1,5 +1,6 @@
 package concurrent;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -10,12 +11,26 @@ public class T26_Map_ConcurrentHashMap {
         Map<String, String> m = new java.util.concurrent.ConcurrentHashMap<>();
     }
     
+    @Before
+    public void init(){
+    
+    }
+    
     @Test
     public void testConstructor() throws IOException {
         Map<String, String> m = new java.util.concurrent.ConcurrentHashMap<>();
         System.in.read();
     }
     
+    /**
+     * 为了加快取模运算,使用二进制运算,例如h&1111
+     * 也是因为使用了二进制运算,因此需要以2为倍数扩容
+     *
+     * $A:二进制取模运算加快了取模速度
+     * $D:但导致hash冲突增加,二进制取模主要是截取二进制后几位,未考虑高位,因此即使高位不同也会hash冲突
+     *
+     * 方案:增加hash 扰动,将高位也考虑进来
+     */
     @Test
     public void testHashMod() {
         String k = "chris";
@@ -41,6 +56,10 @@ public class T26_Map_ConcurrentHashMap {
      * modCount = 86
      * threshold = 96
      * loadFactor = 0.75
+     * <p>
+     * count>tab.length*load.factor时,segment会进行扩展
+     * segment.len=segment.len<<1;扩展为2倍,主要为了方便二进制取模运算
+     * 例如:2^n -1= 1111111..111, 10101100111 & 11111 = 111
      *
      * @throws IOException
      */
