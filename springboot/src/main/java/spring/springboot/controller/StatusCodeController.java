d74a25caa4cce36b0adf62d2a86138799d01a7fe
chris <chrishuhyc@gmail.com>
2021-09-01 21:50:16 +0800

    feat(servlet): container and springboot

--- springboot/src/main/java/spring/springboot/controller/StatusCodeController.java
+++ springboot/src/main/java/spring/springboot/controller/StatusCodeController.java
@@ -1,34 +1,38 @@
 package spring.springboot.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import spring.springboot.exception.ServerException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class StatusCodeController {
-
-  @RequestMapping(value = "/list", method = RequestMethod.GET)
-  public String getBooks(HttpServletRequest request, HttpServletResponse response)
-      throws ServerException {
-    HttpSession session = request.getSession();
-    Subject subject = SecurityUtils.getSubject();
+    
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String getBooks(HttpServletRequest request, HttpServletResponse response)
+            throws ServerException {
+        HttpSession session = request.getSession();
+        Subject subject = SecurityUtils.getSubject();
 //        subject.login(new ShiroSpringWebConfig.MyToken());
-    return "hello";
-  }
-
-  @RequestMapping(value = "/json", method = RequestMethod.POST)
-  @ResponseBody
-  public String jsonObject(@RequestBody JSONObject object) {
-    System.out.println(object.toString());
-    return object.toString();
-  }
+        return "hello";
+    }
+    
+    @RequestMapping(value = "/json", method = RequestMethod.POST)
+    @ResponseBody
+    public String jsonObject(@RequestBody JSONObject object) {
+        System.out.println(object.toString());
+        return object.toString();
+    }
+    
+    @GetMapping(value = "/boolean")
+    @ResponseBody
+    public String jsonObject(@RequestParam(required = false) boolean hi, @RequestParam("type") Integer type) {
+        System.out.println(hi);
+        return "success";
+    }
 }
---
+++ src/main/java/web/servlet/container/tomcat/open/OuterWar.java
@@ -0,0 +1,17 @@
+package web.servlet.container.tomcat.open;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.Set;
+
+public class OuterWar implements ServletContainerInitializer {
+    public static void main(String[] args) {
+    
+    }
+    
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+    
+    }
+}
