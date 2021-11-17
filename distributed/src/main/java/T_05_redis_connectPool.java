ecff4463a2e5819e35ec6923d402166bf7037e49
chrishuhyc <chrishuhyc@gmail.com>
2021-11-17 20:24:35 +0800

    feat(redis): client pool

--- distributed/src/main/java/T_05_redis_connectPool.java
+++ distributed/src/main/java/T_05_redis_connectPool.java
@@ -1,6 +1,11 @@
 import org.junit.Test;
+import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPool;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -39,9 +44,22 @@ public class T_05_redis_connectPool {
     
     /**
      * GenericObjectPool.borrowObject
+     * JedisCluster会池化很多连接，如果连接超时，会随机选择一个节点重连
      */
     @Test
     public void testPool() {
+        String clusterNodes="127.0.0.1:6379;127.0.0.1:6380;127.0.0.1:6381";
+        Set<HostAndPort> jedisClusterNodes = new HashSet<>();
+        String[] nodes = clusterNodes.split(";");
+        String[] var9 = nodes;
+        int var10 = nodes.length;
     
+        for(int var11 = 0; var11 < var10; ++var11) {
+            String node = var9[var11];
+            String[] hostport = node.split(":");
+            jedisClusterNodes.add(new HostAndPort(hostport[0], Integer.valueOf(hostport[1])));
+        }
+    
+        JedisCluster jc = new JedisCluster(jedisClusterNodes, 2, 3, 2, null);
     }
 }
