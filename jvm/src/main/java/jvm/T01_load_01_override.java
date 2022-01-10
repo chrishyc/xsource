3e18e95e33d6276858f1df5b787ced9283283f07
chris <chrishuhyc@gmail.com>
2022-01-10 22:44:17 +0800

    feat(jvm):load method

---
+++ jvm/src/main/java/jvm/T01_load_01_override.java
@@ -0,0 +1,37 @@
+package jvm;
+
+/**
+ * -XX:+TraceClassLoading
+ */
+public class T01_load_01_override {
+
+
+  static abstract class Human {
+  }
+
+  static class Man extends Human {
+  }
+
+  static class Woman extends Human {
+  }
+
+  public void sayHello(Human guy) {
+    System.out.println("hello,guy!");
+  }
+
+  public void sayHello(Man guy) {
+    System.out.println("hello,gentleman!");
+  }
+
+  public void sayHello(Woman guy) {
+    System.out.println("hello,lady!");
+  }
+
+  public static void main(String[] args) {
+    Human man = new Man();
+    Human woman = new Woman();
+    T01_load_01_override sr = new T01_load_01_override();
+    sr.sayHello(man);
+    sr.sayHello(woman);
+  }
+}
---
+++ jvm/src/main/java/jvm/T01_load_05_cover_field.java
@@ -0,0 +1,58 @@
+package jvm;
+
+/**
+ * -XX:+TraceClassLoading
+ */
+public class T01_load_05_cover_field {
+
+
+  static class Father {
+    public int money = 1;
+
+    /**
+     *  0 aload_0(this为son)
+     *  1 invokespecial #1 <java/lang/Object.<init>>
+     *  4 aload_0
+     *  5 iconst_1
+     *  6 putfield #2 <jvm/T01_load_05_cover_field$Father.money>
+     *  9 aload_0
+     * 10 iconst_2
+     * 11 putfield #2 <jvm/T01_load_05_cover_field$Father.money>
+     * 14 aload_0
+     * 15 invokevirtual #3 <jvm/T01_load_05_cover_field$Father.showMeTheMoney>
+     * 18 return
+     */
+    public Father() {
+      money = 2;
+      showMeTheMoney();
+    }
+
+    public void showMeTheMoney() {
+      System.out.println("I am Father, i have $" + money);
+    }
+  }
+
+  static class Son extends Father {
+    public int money = 3;
+
+    public Son() {
+      money = 4;
+      showMeTheMoney();
+    }
+
+    public void showMeTheMoney() {
+      System.out.println("I am Son, i have $" + money);
+    }
+  }
+
+  /**
+   * I am Son, i have $0
+   * I am Son, i have $4
+   * This gay has $2
+   * @param args
+   */
+  public static void main(String[] args) {
+    Father gay = new Son();
+    System.out.println("This gay has $" + gay.money);
+  }
+}
